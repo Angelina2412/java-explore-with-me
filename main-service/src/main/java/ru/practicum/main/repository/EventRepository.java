6bdd0577483a411a1b21b3d6967677ff99a82b45
Angelina <gelikberries@gmail.com>
2025-06-14 16:42:32 +0500

    fixed problems in git

--- main-service/src/main/java/ru/practicum/main/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/main/repository/EventRepository.java
@@ -1,14 +1,10 @@
 package ru.practicum.main.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.main.enums.EventState;
 import ru.practicum.main.model.Event;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,38 +17,5 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByInitiatorId(Long userId, Pageable pageable);
 
     boolean existsByCategoryId(Long categoryId);
-
-    @Query("SELECT e FROM Event e " +
-            "WHERE e.state = 'PUBLISHED' " +
-            "AND (:text IS NULL OR (LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
-            "                        OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%')))) " +
-            "AND (:paid IS NULL OR e.paid = :paid) " +
-            "AND (:rangeStart IS NULL OR e.eventDate >= CAST(:rangeStart AS timestamp)) " +
-            "AND (:rangeEnd IS NULL OR e.eventDate <= CAST(:rangeEnd AS timestamp)) " +
-            "AND (:categoriesSize = 0 OR e.category.id IN :categories)")
-    List<Event> findPublishedEventsWithFilters(@Param("text") String text,
-                                               @Param("categories") List<Long> categories,
-                                               @Param("categoriesSize") int categoriesSize,
-                                               @Param("paid") Boolean paid,
-                                               @Param("rangeStart") LocalDateTime rangeStart,
-                                               @Param("rangeEnd") LocalDateTime rangeEnd,
-                                               Pageable pageable);
-
-    @Query("SELECT e FROM Event e " +
-            "WHERE (:usersSize = 0 OR e.initiator.id IN :users) " +
-            "AND (:statesSize = 0 OR e.state IN :states) " +
-            "AND (:categoriesSize = 0 OR e.category.id IN :categories) " +
-            "AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) " +
-            "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd)")
-    Page<Event> searchByAdminFilters(@Param("users") List<Long> users,
-                                     @Param("usersSize") int usersSize,
-                                     @Param("states") List<String> states,
-                                     @Param("statesSize") int statesSize,
-                                     @Param("categories") List<Long> categories,
-                                     @Param("categoriesSize") int categoriesSize,
-                                     @Param("rangeStart") LocalDateTime rangeStart,
-                                     @Param("rangeEnd") LocalDateTime rangeEnd,
-                                     Pageable pageable);
-
 }
 
